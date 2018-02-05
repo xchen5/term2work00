d8ad32dd95b9e91fc5eb6f85ce80d1eae40818a1
xchen5 <xchen5@stuy.edu>
2018-02-05 14:04:34 -0500

    in progress

--- canvas.js
+++ canvas.js
@@ -1,17 +1,30 @@
 var c = document.getElementById("can");
 var ctx = c.getContext("2d");
 
+var counter = 0;
 var toCircle = function(e){
     ctx.beginPath();
     var x = e.clientX;
     var y = e.clientY;
-    ctx.arc(x,y,10,0,2 * Math.PI);
     ctx.fillStyle= "red";
-    ctx.fill();
+    if (counter === 0){
+	ctx.arc(x,y,10,0,2 * Math.PI);
+	ctx.fill();
+	ctx.moveTo(x,y);
+	counter += 1;
+    }
+    else{
+	ctx.arc(x,y,10,0,2 * Math.PI);
+	ctx.fill();
+	ctx.lineTo(x,y);
+	ctx.stroke();
+	counter += 1;
+    }
 };
 
 var toClear = function(e){
-  ctx.clearRect(0,0, 500,500);
+    ctx.clearRect(0,0, 500,500);
+    counter
 };
 
 var clearButton = document.getElementById("clear");
@@ -22,8 +35,8 @@ var toggleButton = document.getElementById("toggle");
 
 var toSquare = function(e){
   ctx.beginPath();
-  var x = e.clientX;
-  var y = e.clientY;
+  var x = e.offsetX;
+  var y = e.offsetY;
   ctx.rect(x,y,30,30);
   ctx.fillStyle = "blue";
   ctx.fill();
